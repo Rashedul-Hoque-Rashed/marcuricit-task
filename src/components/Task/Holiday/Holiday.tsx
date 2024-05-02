dfc60e644c3cc47a00f1fb244fd71d24a300755f
Rashed <rashedulhoqueceo@gmail.com>
2024-05-02 13:50:37 +0600

    added holiday list table on holiday page

--- src/components/Task/Holiday/Holiday.tsx
+++ src/components/Task/Holiday/Holiday.tsx
@@ -1,9 +1,223 @@
-import React from 'react';
+import { Button, Card, Col, Dropdown, Modal, Row } from 'react-bootstrap';
+import './Holiday.scss';
+import Table from '../../Table';
+import { useState } from 'react';
+import { holidayData } from './data'
+import { Form } from 'react-bootstrap';
 
 const Holiday = () => {
+
+
+    const [showCenteredModal2, setShowCenteredModal2] = useState<boolean>(false);
+    const [showCenteredModal3, setShowCenteredModal3] = useState<boolean>(false);
+
+
+    const columns = [
+        {
+            Header: "ID",
+            accessor: "id",
+            sort: true,
+        },
+        {
+            Header: "Title",
+            accessor: "title",
+            sort: true,
+        },
+        {
+            Header: "Holiday Date",
+            accessor: "date",
+            sort: false,
+        },
+        {
+            Header: "Day",
+            accessor: "day",
+            sort: true,
+        },
+        {
+            Header: "Action",
+            accessor: "action",
+            Cell: () => {
+                const [showCenteredModal, setShowCenteredModal] = useState(false);
+
+                return (
+                    <>
+                        <Dropdown className="action float-end" align="end">
+                            <Dropdown.Toggle as="a" className="cursor-pointer arrow-none">
+                                <i className="uil uil-ellipsis-v fs-16 text-black"></i>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => setShowCenteredModal2(true)}>
+                                    <i className="uil uil-edit-alt me-2"></i>Edit
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item className="text-danger" onClick={() => setShowCenteredModal(true)}>
+                                    <i className="uil uil-trash me-2"></i>Delete
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+
+                        <Modal
+                            show={showCenteredModal2}
+                            onHide={() => setShowCenteredModal2(false)}
+                            centered
+                        >
+                            <Modal.Header closeButton>
+                                <h2>Edit Holiday</h2>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="title">Holiday Name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="title"
+                                            id="title"
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group className="">
+                                        <Form.Label htmlFor="date">Holiday Date</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            name="date"
+                                            id="date"
+                                        />
+                                    </Form.Group>
+
+
+                    
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer className='mx-auto'>
+                            <Button variant="primary" type="submit">
+                                        Submit
+                                    </Button>
+                            </Modal.Footer>
+                        </Modal>
+
+
+                        <Modal
+                            show={showCenteredModal}
+                            onHide={() => setShowCenteredModal(false)}
+                            centered
+                        >
+                            <Modal.Header closeButton>
+                            </Modal.Header>
+                            <Modal.Body className='text-center'>
+                                <h3>Delete Employee</h3>
+                                <h6>Are you sure want to delete?</h6>
+                            </Modal.Body>
+                            <Modal.Footer className='mx-auto'>
+                                <Button variant="danger">Delete</Button>
+                                <Button variant="light" onClick={() => setShowCenteredModal(false)}>
+                                    Cancel
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </>
+                );
+            },
+            sort: false,
+        },
+    ];
+
+    const sizePerPageList = [
+        {
+            text: "5",
+            value: 5,
+        },
+        {
+            text: "10",
+            value: 10,
+        },
+        {
+            text: "25",
+            value: 25,
+        },
+        {
+            text: "All",
+            value: holidayData.length,
+        },
+    ];
+
+
+
     return (
-        <div>
-            holiday
+        <div className='holiday'>
+            <div className="header">
+                <div className="textContainer">
+                    <h2>Holiday</h2>
+                    <h4>Dashboard / Holiday</h4>
+                </div>
+                <div className="actionContainer">
+                    <button onClick={() => setShowCenteredModal3(true)}>+ Add Holiday</button>
+                </div>
+
+
+                <Modal
+                            show={showCenteredModal3}
+                            onHide={() => setShowCenteredModal3(false)}
+                            centered
+                        >
+                            <Modal.Header closeButton>
+                                <h2>Add Holiday</h2>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="title">Holiday Name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="title"
+                                            id="title"
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group className="">
+                                        <Form.Label htmlFor="date">Holiday Date</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            name="date"
+                                            id="date"
+                                        />
+                                    </Form.Group>
+
+
+                    
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer className='mx-auto'>
+                            <Button variant="primary" type="submit">
+                                        Submit
+                                    </Button>
+                            </Modal.Footer>
+                        </Modal>
+
+
+
+            </div>
+
+
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Table
+                                columns={columns}
+                                data={holidayData}
+                                pageSize={5}
+                                sizePerPageList={sizePerPageList}
+                                isSortable={true}
+                                pagination={true}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+
+
+
         </div>
     );
 };
---
+++ src/components/Task/Holiday/data.tsx
@@ -0,0 +1,51 @@
+export const holidayData = [
+    {
+      "id": 1,
+      "title": "New Year's Day",
+      "date": "2024-01-01",
+      "day": "Tuesday"
+    },
+    {
+      "id": 2,
+      "title": "Independence Day",
+      "date": "2024-07-04",
+      "day": "Thursday"
+    },
+    {
+      "id": 3,
+      "title": "Christmas Day",
+      "date": "2024-12-25",
+      "day": "Wednesday"
+    },
+    {
+      "id": 4,
+      "title": "Thanksgiving Day",
+      "date": "2024-11-28",
+      "day": "Thursday"
+    },
+    {
+      "id": 5,
+      "title": "Labor Day",
+      "date": "2024-09-02",
+      "day": "Monday"
+    },
+    {
+      "id": 6,
+      "title": "Memorial Day",
+      "date": "2024-05-27",
+      "day": "Monday"
+    },
+    {
+      "id": 7,
+      "title": "Veterans Day",
+      "date": "2024-11-11",
+      "day": "Monday"
+    },
+    {
+      "id": 8,
+      "title": "Easter Sunday",
+      "date": "2024-04-21",
+      "day": "Sunday"
+    }
+  ]
+  
