9a3515439c84c5dd88fad34990dbd1f399b140df
Rashed <rashedulhoqueceo@gmail.com>
2024-05-02 16:36:53 +0600

    added leaves data table on leaves employee page

--- src/components/Task/LeavesEmployee/LeavesEmployee.tsx
+++ src/components/Task/LeavesEmployee/LeavesEmployee.tsx
@@ -1,9 +1,325 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card, Col, Dropdown, Form, Modal, Row } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import './LeavesEmployee.scss'
+import {leavesData} from './data';
+import Table from '../../Table';
 
 const LeavesEmployee = () => {
+    const [showCenteredModal4, setShowCenteredModal4] = useState<boolean>(false);
+    const [showCenteredModal5, setShowCenteredModal5] = useState<boolean>(false);
+
+
+
+    const columns = [
+        {
+            Header: "Leave Type",
+            accessor: "leaveType",
+            sort: false,
+        },
+        {
+            Header: "From",
+            accessor: "from",
+            sort: true,
+        },
+        {
+            Header: "To",
+            accessor: "to",
+            sort: true,
+        },
+        {
+            Header: "No Of Days",
+            accessor: "noOfDay",
+            sort: true,
+        },
+        {
+            Header: "Reason",
+            accessor: "reason",
+            sort: false,
+        },
+        {
+            Header: "Status",
+            accessor: "status",
+            sort: true,
+        },
+        {
+            Header: "Action",
+            accessor: "action",
+            Cell: () => {
+                const [showCenteredModal, setShowCenteredModal] = useState(false);
+
+                return (
+                    <>
+                        <Dropdown className="action float-end" align="end">
+                            <Dropdown.Toggle as="a" className="cursor-pointer arrow-none">
+                                <i className="uil uil-ellipsis-v fs-16 text-black"></i>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item onClick={() => setShowCenteredModal4(true)}>
+                                    <i className="uil uil-edit-alt me-2"></i>Edit
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item className="text-danger" onClick={() => setShowCenteredModal(true)}>
+                                    <i className="uil uil-trash me-2"></i>Delete
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+
+                        <Modal
+                            show={showCenteredModal4}
+                            onHide={() => setShowCenteredModal4(false)}
+                            centered
+                        >
+                            <Modal.Header closeButton>
+                                <h2>Edit Leaves</h2>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>
+                                            Leaves Type
+                                        </Form.Label>
+                                        <Col>
+                                            <Form.Select>
+                                                <option>Casual Leave</option>
+                                                <option>Medical Leave</option>
+                                                <option>Loss of Pay</option>
+                                            </Form.Select>
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="title">From</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            name="from"
+                                            id="from"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="title">To</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            name="to"
+                                            id="to"
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="date">Number Of Day</Form.Label>
+                                        <Form.Control
+                                            type="number"
+                                            name="noOfDay"
+                                            id="noOfDay"
+                                            min={0}
+                                            max={12}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="date">Remaining Leaves</Form.Label>
+                                        <Form.Control
+                                            type="number"
+                                            name="noOfDay"
+                                            id="noOfDay"
+                                            max={12}
+                                            min={0}
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="example-textarea">
+                                            Leave Reason
+                                        </Form.Label>
+                                        <Col>
+                                            <Form.Control as="textarea" rows={3} id="example-textarea" />
+                                        </Col>
+                                    </Form.Group>
+
+
+
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer className='mx-auto'>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+
+
+                        <Modal
+                            show={showCenteredModal}
+                            onHide={() => setShowCenteredModal(false)}
+                            centered
+                        >
+                            <Modal.Header closeButton>
+                            </Modal.Header>
+                            <Modal.Body className='text-center'>
+                                <h3>Delete Employee</h3>
+                                <h6>Are you sure want to delete?</h6>
+                            </Modal.Body>
+                            <Modal.Footer className='mx-auto'>
+                                <Button variant="danger">Delete</Button>
+                                <Button variant="light" onClick={() => setShowCenteredModal(false)}>
+                                    Cancel
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </>
+                );
+            },
+            sort: false,
+        },
+    ];
+
+    const sizePerPageList = [
+        {
+            text: "5",
+            value: 5,
+        },
+        {
+            text: "10",
+            value: 10,
+        },
+        {
+            text: "25",
+            value: 25,
+        },
+        {
+            text: "All",
+            value: leavesData.length,
+        },
+    ];
+
+
+
+
     return (
-        <div>
-            LeavesEmployee
+        <div className='leavesEmployee'>
+            <div className="header">
+                <div className="textContainer">
+                    <h2>Leaves</h2>
+                    <h4>Dashboard / Leaves</h4>
+                </div>
+                <div className="actionContainer">
+                    <button onClick={() => setShowCenteredModal5(true)}>+ Add Leave</button>
+                </div>
+
+                <Modal
+                            show={showCenteredModal5}
+                            onHide={() => setShowCenteredModal5(false)}
+                            centered
+                        >
+                            <Modal.Header closeButton>
+                                <h2>Add Leaves</h2>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>
+                                            Leaves Type
+                                        </Form.Label>
+                                        <Col>
+                                            <Form.Select>
+                                                <option>Casual Leave</option>
+                                                <option>Medical Leave</option>
+                                                <option>Loss of Pay</option>
+                                            </Form.Select>
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="title">From</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            name="from"
+                                            id="from"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="title">To</Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            name="to"
+                                            id="to"
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="date">Number Of Day</Form.Label>
+                                        <Form.Control
+                                            type="number"
+                                            name="noOfDay"
+                                            id="noOfDay"
+                                            min={0}
+                                            max={12}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="date">Remaining Leaves</Form.Label>
+                                        <Form.Control
+                                            type="number"
+                                            name="noOfDay"
+                                            id="noOfDay"
+                                            max={12}
+                                            min={0}
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label htmlFor="example-textarea">
+                                            Leave Reason
+                                        </Form.Label>
+                                        <Col>
+                                            <Form.Control as="textarea" rows={3} id="example-textarea" />
+                                        </Col>
+                                    </Form.Group>
+
+
+
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer className='mx-auto'>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+
+            </div>
+            <div className="boxes">
+                <div className="box">
+                    <h4>Annual Leave</h4>
+                    <h2>12</h2>
+                </div>
+                <div className="box">
+                    <h4>Medical Leaves</h4>
+                    <h2>3</h2>
+                </div>
+                <div className="box">
+                    <h4>Others Leaves</h4>
+                    <h2>4</h2>
+                </div>
+                <div className="box">
+                    <h4>Remaining Leave</h4>
+                    <h2>5</h2>
+                </div>
+            </div>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Table
+                                columns={columns}
+                                data={leavesData}
+                                pageSize={5}
+                                sizePerPageList={sizePerPageList}
+                                isSortable={true}
+                                pagination={true}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
 };
--- src/components/Task/LeavesEmployee/data.tsx
+++ src/components/Task/LeavesEmployee/data.tsx
@@ -1,19 +1,7 @@
-import avatar1 from '../../../assets/images/users/avatar-1.jpg';
-import avatar2 from '../../../assets/images/users/avatar-2.jpg';
-import avatar3 from '../../../assets/images/users/avatar-3.jpg';
-import avatar4 from '../../../assets/images/users/avatar-4.jpg';
-import avatar5 from '../../../assets/images/users/avatar-5.jpg';
-import avatar6 from '../../../assets/images/users/avatar-6.jpg';
-import avatar7 from '../../../assets/images/users/avatar-7.jpg';
-import avatar8 from '../../../assets/images/users/avatar-8.jpg';
-import avatar9 from '../../../assets/images/users/avatar-9.jpg';
-import avatar10 from '../../../assets/images/users/avatar-10.jpg';
-
 export const leavesData = [
     {
       "id": 1,
       "name": "John Doe",
-      "img": avatar1,
       "leaveType": "Annual Leave",
       "from": "2024-06-01",
       "to": "2024-06-03",
@@ -24,7 +12,6 @@ export const leavesData = [
     {
       "id": 2,
       "name": "Alice Smith",
-      "img": avatar2,
       "leaveType": "Sick Leave",
       "from": "2024-07-10",
       "to": "2024-07-11",
@@ -35,7 +22,6 @@ export const leavesData = [
     {
       "id": 3,
       "name": "Emily Johnson",
-      "img": avatar3,
       "leaveType": "Maternity Leave",
       "from": "2024-09-15",
       "to": "2024-12-15",
@@ -46,7 +32,6 @@ export const leavesData = [
     {
       "id": 4,
       "name": "Michael Brown",
-      "img": avatar4,
       "leaveType": "Emergency Leave",
       "from": "2024-10-20",
       "to": "2024-10-20",
@@ -57,7 +42,6 @@ export const leavesData = [
     {
       "id": 5,
       "name": "Emma Wilson",
-      "img": avatar5,
       "leaveType": "Personal Leave",
       "from": "2024-11-05",
       "to": "2024-11-06",
@@ -68,7 +52,6 @@ export const leavesData = [
     {
       "id": 6,
       "name": "William Miller",
-      "img": avatar6,
       "leaveType": "Annual Leave",
       "from": "2024-12-20",
       "to": "2024-12-30",
@@ -79,7 +62,6 @@ export const leavesData = [
     {
       "id": 7,
       "name": "Sophia Wilson",
-      "img": avatar7,
       "leaveType": "Sick Leave",
       "from": "2024-03-05",
       "to": "2024-03-07",
@@ -90,7 +72,6 @@ export const leavesData = [
     {
       "id": 8,
       "name": "James Taylor",
-      "img": avatar8,
       "leaveType": "Emergency Leave",
       "from": "2024-08-10",
       "to": "2024-08-12",
@@ -101,7 +82,6 @@ export const leavesData = [
     {
       "id": 9,
       "name": "Olivia Anderson",
-      "img": avatar9,
       "leaveType": "Personal Leave",
       "from": "2024-04-25",
       "to": "2024-04-26",
@@ -112,7 +92,6 @@ export const leavesData = [
     {
       "id": 10,
       "name": "David Martinez",
-      "img": avatar10,
       "leaveType": "Maternity Leave",
       "from": "2024-11-20",
       "to": "2025-02-20",
